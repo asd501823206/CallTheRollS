a76eb7f1d90994adfeb5a92374078dbfaaf762dd
cm1996 <chenmo1996@outlook.com>
2016-01-19 08:08:56 +0800

    Revert "01.18 16:44 still got mistakes"
    This reverts commit 3ce7a5c70cc40475f4cc3c662c6fd4e494d4a96f.

---
+++ android/DianMing/app/src/main/java/com/dream_art/dianming/Person.java
@@ -0,0 +1,16 @@
+package com.dream_art.dianming;
+
+/**
+ * Created by lenovo on 2016/1/18.
+ */
+public class Person {
+    int mFlag;
+    String mUserId;
+    String mUserPwd;
+
+    public Person(int flag,String userId,String userPwd){
+        mFlag = flag;
+        mUserId = userId;
+        mUserPwd = userPwd;
+    }
+}
